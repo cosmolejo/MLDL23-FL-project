6797d0fe26fd2bd34b71c5ec6753a8c3a16d0d91
Alejandro Mesa <almego95@gmail.com>
2023-11-25 18:53:39 +0100

    Client made by Albi

--- entities/client.py
+++ entities/client.py
@@ -4,13 +4,15 @@ import torch
 from torch import optim, nn
 from collections import defaultdict
 from torch.utils.data import DataLoader
+import threading
 
 from utils.utils import HardNegativeMining, MeanReduction
 
 
 class Client:
 
-    def __init__(self, args, dataset, model, test_client=False):
+    def __init__(self, args, dataset, model, optimizer, test_client=False):
+        # putting the optimizer as an input parameter
         self.args = args
         self.dataset = dataset
         self.name = self.dataset.client_name
@@ -18,6 +20,7 @@ class Client:
         self.train_loader = DataLoader(self.dataset, batch_size=self.args.bs, shuffle=True, drop_last=True) \
             if not test_client else None
         self.test_loader = DataLoader(self.dataset, batch_size=1, shuffle=False)
+        self.optimizer = optimizer
         self.criterion = nn.CrossEntropyLoss(ignore_index=255, reduction='none')
         self.reduction = HardNegativeMining() if self.args.hnm else MeanReduction()
 
@@ -38,15 +41,37 @@ class Client:
             return self.model(images)
         raise NotImplementedError
 
-    def run_epoch(self, cur_epoch, optimizer):
+    def run_epoch(self):
         """
         This method locally trains the model with the dataset of the client. It handles the training at mini-batch level
         :param cur_epoch: current epoch of training
         :param optimizer: optimizer used for the local training
         """
+        # There is also scheduler for the learning rate that we will put later.
+        # self.optim_scheduler.step()
         for cur_step, (images, labels) in enumerate(self.train_loader):
-            # TODO: missing code here!
-            raise NotImplementedError
+            images = images.cuda()
+            labels = labels.cuda()
+
+            self.optimizer.zero_grad()
+
+            outputs = self.model(images)
+
+            loss = self.criterion(outputs, labels)
+
+            loss.backward()
+            self.optimizer.step()
+
+            predictions = torch.argmax(outputs, dim=1)
+
+            correct_predictions = torch.sum(predictions == labels).item()
+            tot_correct_predictions += correct_predictions
+            epoch_loss += loss.item()
+
+        avg_loss = epoch_loss / self.iter_per_epoch
+        accuracy = tot_correct_predictions / self.len_dataset * 100
+
+        return avg_loss, accuracy
 
     def train(self):
         """
@@ -54,19 +79,36 @@ class Client:
         (by calling the run_epoch method for each local epoch of training)
         :return: length of the local dataset, copy of the model parameters
         """
-        # TODO: missing code here!
+        # initial_model_params = copy.deepcopy(self.model.state_dict())
+        # maybe it is needed
+
         for epoch in range(self.args.num_epochs):
-            # TODO: missing code here!
-            raise NotImplementedError
+            print(f"tid={str(threading.get_ident())[-7:]} - k_id={self.idx}: START EPOCH={epoch + 1}/{self.num_epochs}")
+            avg_loss, train_accuracy = self.run_epoch()
+            print(
+                f"tid={str(threading.get_ident())[-7:]} - k_id={self.idx}: END   EPOCH={epoch + 1}/{self.num_epochs} - ",
+                end="")
+            print(f"Loss={round(avg_loss, 3)}, Accuracy={round(train_accuracy, 2)}%")
+
+        return self.model.state_dict()
 
     def test(self, metric):
         """
         This method tests the model on the local dataset of the client.
         :param metric: StreamMetric object
         """
-        # TODO: missing code here!
+        correct = 0
+        total = 0
         with torch.no_grad():
             for i, (images, labels) in enumerate(self.test_loader):
-                # TODO: missing code here!
-                raise NotImplementedError
+                images = images.cuda()
+                labels = labels.cuda()
+
+                outputs = self.model(images)
+
+                _, predicted = torch.max(outputs.data, 1)
+
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
                 self.update_metric(metric, outputs, labels)
